525f16ce1a235816f016c235fc155d836be8d37a
MiracleRo <741556866@qq.com>
2019-06-16 13:43:34 +0800

    完成注册样式

--- src/component/SignUp/SignUp.tsx
+++ src/component/SignUp/SignUp.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Input, Button, Icon } from 'antd';
 import axios from '../../config/axios'
 import { Link } from 'react-router-dom'
+import './SignUp.scss'
 
 interface ISignUp {
   account: string,
@@ -43,10 +44,11 @@ class SignUp extends Component<any, ISignUp> {
     const { account, password, passwordConfirmation } = this.state
     return (
       <div className="sign-up-wrapper">
+        <h1>番茄时钟注册</h1>
         <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="请输入注册账号" value={account} allowClear={true} onChange={(e) => this.onChange(e.target.value, 'account')} />
         <Input.Password prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} value={password} placeholder="请输入密码" onChange={(e) => this.onChange(e.target.value, 'password')} />
         <Input.Password prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} value={passwordConfirmation} placeholder="请确认密码" onChange={(e) => this.onChange(e.target.value, 'passwordConfirmation')}  />
-        <Button type="primary" onClick={this.submit}>注册</Button>
+        <Button className="signUpButton" type="primary" onClick={this.submit}>注册</Button>
         <p>或者立即<Link to="/login">登录</Link></p>
       </div>
     );
