5cb6e5de1b25e97b24eff1f9e443b3b2df2e29a7
MiracleRo <741556866@qq.com>
2019-06-30 19:45:55 +0800

    统计折线图

--- src/component/Statistics/Statistics.tsx
+++ src/component/Statistics/Statistics.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import './Statistics.scss'
 import { connect } from 'react-redux'
+import Polygon from './Polygon'
+import {format} from "date-fns";
+import _ from 'lodash'
+import './Statistics.scss'
 
 interface IStatProps {
   todos: any[]
@@ -10,6 +13,12 @@ class Statistics extends Component<IStatProps> {
   get finishedTodos () {
     return this.props.todos.filter(t => t.completed && !t.deleted)
   }
+
+  get dailyTodos(){
+		return  _.groupBy(this.finishedTodos,(todo)=>{
+			return format(todo.updated_at,'YYYY-MM-D')
+		})
+	}
 
   render() {
     return (
@@ -21,6 +30,7 @@ class Statistics extends Component<IStatProps> {
 					<li>
 						任务历史
 						累计完成{this.finishedTodos.length}个任务
+            <Polygon data={this.dailyTodos} totalFinishedCount={this.finishedTodos.length} />
 					</li>
 				</ul>
 			</div>
