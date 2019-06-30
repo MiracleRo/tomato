177852973f299cf79e07e74744dbc868b6a2007e
MiracleRo <741556866@qq.com>
2019-06-30 12:03:53 +0800

    完成初始化tomato

--- src/component/Tomatos/Tomatos.tsx
+++ src/component/Tomatos/Tomatos.tsx
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
 import './Tomato.scss'
 import TomatosAction from './TomatosAction'
+import axios from '../../config/axios'
+import { connect } from 'react-redux'
+import { initTomatoes, addTomatoes } from '../../redux/action/tomatoes'
 
-class Tomatos extends Component<{}> {
+interface ITOMATOES {
+  addTomatoes: (payload: any) => void
+  initTomatoes: (payload: any) => void
+  tomatoes: any[]
+}
+
+class Tomatos extends Component<ITOMATOES, any> {
+
+  componentWillMount () {
+    this.init()
+  }
+
+  get unfinishedTomato () {
+    return this.props.tomatoes.filter(item => !item.description && !item.ended_at)[0]
+  }
+
+  startAction = async () => {
+    const res = await axios.post('tomatoes', {durtion: 1500000})
+    this.props.addTomatoes(res.data.resource)
+    console.log(this.props.tomatoes)
+  }
+
+  init = async () => {
+    const res = await axios.get('tomatoes')
+    this.props.initTomatoes(res.data.resources)
+  }
+
   render() {
     return (
       <div className="tomato">
-        <TomatosAction />
+        <TomatosAction unfinishedTomato={this.unfinishedTomato} startAction={this.startAction} />
       </div>
     );
   }
 }
 
-export default Tomatos
+const mapStateToProps = (state: any, ownProps: any) => ({
+  tomatoes: state.tomatoes,
+	...ownProps
+})
+
+
+const mapDispatchToProps = {
+  initTomatoes,
+  addTomatoes 
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tomatos)
--- src/component/Tomatos/TomatosAction.tsx
+++ src/component/Tomatos/TomatosAction.tsx
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import { Button } from "antd"
-import axios from 'src/config/axios'
 
-class TomatosAction extends Component<{}> {
+interface ITomatoActionProps {
+  unfinishedTomato: any,
+  startAction: () => void
+}
+
+class TomatosAction extends Component<ITomatoActionProps> {
   constructor (prop: any) {
     super(prop)
-  }
-
-  async startAction () {
-    const res = await axios.post('tomatoes', {durtion: 1500000})
-    console.log(res)
   }
 
   render() {
     return (
       <div className="tomatoAction">
-        <Button onClick={this.startAction}>开始番茄</Button>
+        <Button onClick={(e:any) => {this.props.startAction()}}>开始番茄</Button>
       </div>
     );
   }
