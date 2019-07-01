621359f720a2675f39d0dc622abcc5f78ae81c59
MiracleRo <741556866@qq.com>
2019-07-01 11:23:56 +0800

    完成部署

--- src/config/history.ts
+++ src/config/history.ts
@@ -1,4 +1,14 @@
 import { createBrowserHistory } from 'history'
-const history = createBrowserHistory()
+
+const ENV = process.env.NODE_ENV
+let publicUrl  = ''
+
+if (ENV === 'development') {
+  publicUrl = ''
+} else if (ENV === 'production'){
+  publicUrl = '/tomato'
+}
+
+const history = createBrowserHistory({basename: publicUrl})
 
 export default history
